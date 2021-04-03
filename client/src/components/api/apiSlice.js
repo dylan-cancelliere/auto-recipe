e6402595e94df327ab10ef8a77fec284a6427db8
Dylan Cancelliere <dycancel@gmail.com>
2021-04-03 17:05:57 -0400

    Defines search endpoint

--- client/src/components/api/apiSlice.js
+++ client/src/components/api/apiSlice.js
@@ -1,42 +1,33 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { EDAMAM_API, EDAMAM_ID } from '../../secrets';
-
-const BASE_URL = 'https://api.edamam.com/';
 
 export const recipeSearch = createAsyncThunk(
 	'api/fetchRecipes',
 	async (queryText, thunkAPI) => {
-		console.log('Reached thunk');
-		const url = `${BASE_URL}/search?q=${encodeURIComponent(
-			queryText
-		)}&app_id=${EDAMAM_ID}&app_key=${EDAMAM_API}`;
-		console.log(`URL: ${url}`);
-		let response = await fetch(url, {
-			method: 'GET',
-			headers: {
-				'Accept-Encoding': 'gzip',
-			},
-		}).catch((err) => {
-			console.log('API failed');
-		});
-		response = await response.json();
-		console.log('End of thunk');
-		return response.data;
+		// const url = `${BASE_URL}/search?q=${encodeURIComponent(
+		// 	queryText
+		// )}&app_id=${EDAMAM_ID}&app_key=${EDAMAM_API}`;
+		// let response = await fetch(url, {
+		// 	method: 'GET',
+		// 	headers: {
+		// 		'Accept-Encoding': 'gzip',
+		// 	},
+		// }).catch((err) => {
+		// 	console.log('API failed');
+		// });
+		// response = await response.json();
+		// return response;
 	}
 );
 
 export const apiSlice = createSlice({
 	name: 'api',
 	initialState: {
-		dataFromBackend: 'Nothing yet',
+		searchResults: {},
 	},
 	reducers: {},
 	extraReducers: {
 		[recipeSearch.fulfilled]: (state, action) => {
-			console.log('Start of reducer');
-			console.log(state);
-			console.log(action.payload);
-			console.log('End of reducer');
+			state.searchResults = action.payload;
 		},
 	},
 });
--- server.js
+++ server.js
@@ -1,22 +1,67 @@
 const express = require('express');
-
+const https = require('https');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 5000;
 
-let data = [];
+const EDAMAM_API = require('./secrets').EDAMAM_API;
+const EDAMAM_ID = require('./secrets').EDAMAM_ID;
+const BASE_URL = 'api.edamam.com';
 
-app.use(express.urlencoded());
+const getIngredients = () => {
+	let result, data;
+	try {
+		data = fs.readFileSync(
+			'/home/ripto/auto-recipe/ingredients.txt',
+			'utf8'
+		);
+	} catch (error) {
+		console.error(error);
+		return;
+	}
+	result = new Set(
+		data
+			.substring(1, data.length - 1)
+			.replace(/'/g, '')
+			.split(', ')
+	);
+	return result;
+};
+
+const ingredients = getIngredients();
+
 app.use(express.json());
 
-app.get('/api/dataa', (req, res) => {
-	res.send({ data: 'abc123' });
-});
+app.get('/search/:query', async (request, response) => {
+	const options = {
+		host: BASE_URL,
+		path: `/search?q=${encodeURIComponent(
+			request.params.query
+		)}&app_id=${EDAMAM_ID}&app_key=${EDAMAM_API}`,
+		method: 'GET',
+		headers: { 'content-type': 'application/json' },
+	};
 
-app.put('/api/data', (req, res) => {
-	console.log(req.body);
-	console.log('this shit broke');
-	res.send({ data: 'Message recieved' });
-	// res.send();
+	const req = https.request(options, (res) => {
+		console.log('statusCode:', res.statusCode);
+		console.log('headers:', res.headers);
+
+		let data = [];
+
+		res.on('data', (chunk) => {
+			data.push(chunk);
+		}).on('end', () => {
+			let result = JSON.parse(Buffer.concat(data));
+			console.log(result);
+			response.send(result);
+		});
+	});
+
+	req.on('error', (e) => {
+		console.error('Error:', e);
+	});
+
+	req.end();
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
