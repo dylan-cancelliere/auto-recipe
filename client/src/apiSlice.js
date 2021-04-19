ac1c9c7a2b1e78accc2d77f1460e5e7860981923
Dylan Cancelliere <dycancel@gmail.com>
2021-04-19 15:37:07 -0400

    Redux to handle backend calls

--- client/src/apiSlice.js
+++ client/src/apiSlice.js
@@ -3,7 +3,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 const BASE_URL = 'http://localhost:5000';
 
 export const recipeSearch = createAsyncThunk(
-	'api/fetchRecipes',
+	'api/searchRecipes',
 	async (queryText, thunkAPI) => {
 		const url = `${BASE_URL}/search/${queryText}`;
 
@@ -19,19 +19,61 @@ export const recipeSearch = createAsyncThunk(
 	}
 );
 
+export const setPantry = createAsyncThunk(
+	'api/setPantry',
+	async (pantry, thunkAPI) => {
+		const url = `${BASE_URL}/pantry`;
+		let response = await fetch(url, {
+			method: 'PUT',
+			body: {
+				user: pantry.user,
+				pantry: pantry.pantry,
+			},
+			headers: {},
+		}).catch((error) => {
+			console.error('Error:', error);
+		});
+
+		response = await response.json();
+		return response;
+	}
+);
+
+export const getPantry = createAsyncThunk(
+	'api/getPantry',
+	async (user, thunkAPI) => {
+		const url = `${BASE_URL}/pantry`;
+		let response = await fetch(url, {
+			method: 'GET',
+			body: { user },
+			headers: {},
+		}).catch((error) => {
+			console.error('Error:', error);
+		});
+
+		response = await response.json();
+		return response;
+	}
+);
+
 export const apiSlice = createSlice({
 	name: 'api',
 	initialState: {
 		searchResults: {},
+		pantryResults: {},
 	},
 	reducers: {},
 	extraReducers: {
 		[recipeSearch.fulfilled]: (state, action) => {
 			state.searchResults = action.payload;
 		},
+		[getPantry.fulfilled]: (state, action) => {
+			state.pantryResults = action.payload;
+		},
 	},
 });
 
 export const selectSearchResults = (state) => state.api.searchResults;
+export const selectPantryResults = (state) => state.api.pantryResults;
 
 export default apiSlice.reducer;
